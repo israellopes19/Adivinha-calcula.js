8bcfc924a14eae79ca487a76aea0e26914ba6e61
Israellopes <143666711+israellopes19@users.noreply.github.com>
2024-05-27 19:41:20 -0300

    Add files via upload

--- calculadora.js
+++ calculadora.js
@@ -12,22 +12,31 @@ while (true){
     if(escolha == '+') {
         var num1 = parseFloat(entrada.question('1° Numero: '));
         var num2 = parseFloat(entrada.question('2° Numero: '));
-        console.log('Adição: ${num1 + num2}';)
+        console.log(`Adição: ${num1 + num2}`);
     }else if(escolha == '-' ){
         var num1 = parseFloat(entrada.question('1° Numero: '));
         var num2 = parseFloat(entrada.question('2° Numero: '));
-        console.log('Adição: ${num1 + num2}';)
-    }else if(escolha == '-' ){    var num1 = parseFloat(entrada.question('1° Numero: '));
-    var num2 = parseFloat(entrada.question('2° Numero: '));
-    console.log('Adição: ${num1 + num2}';)
-    }else if(escolha == '-' ){    var num1 = parseFloat(entrada.question('1° Numero: '));
-    var num2 = parseFloat(entrada.question('2° Numero: '));
-    console.log('Adição: ${num1 + num2}';)
-    }else if(escolha == '-' ){    var num1 = parseFloat(entrada.question('1° Numero: '));
-      var num2 = parseFloat(entrada.question('2° Numero: '));
-console.log('Adição: ${num1 + num2}';)
-}else if(escolha == '-' ){
-    }
+        console.log(`Subtração: ${num1 - num2}`);
+    }else if(escolha == '*' ){    
+        var num1 = parseFloat(entrada.question('1° Numero: '));
+        var num2 = parseFloat(entrada.question('2° Numero: '));
+    console.log(`Multiplicação: ${num1 * num2}`);
+    }else if(escolha == '/' ){    
+        var num1 = parseFloat(entrada.question('1° Numero: '));
+        var num2 = parseFloat(entrada.question('2° Numero: '));
+    console.log(`Divisão: ${num1 / num2}`);
+    }else if(escolha == '**' ){    
+        var num1 = parseFloat(entrada.question('1° Numero: '));
+        var num2 = parseFloat(entrada.question('2° Numero: '));
+    console.log(`Potenciação: ${Math.pow(num1,  num2)}`);
+    }else if(escolha == '%' ){
+        var num1 = parseFloat(entrada.question('1° Numero:'));
+        console.log(`Radiciação: ${Math.sqrt}`);
+    }else{
+        console.log('Operação inválida!!!');
+      }
+    
+
 
 
 
